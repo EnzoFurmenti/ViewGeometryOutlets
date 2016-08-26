93057607d10c5010026d3908ecd7d20acdbea830
EnzoFurmenti <ducheenzo@mail.ru>
2016-08-26 08:33:21 +0300

    Initial Commit

---
+++ ViewGeometryOutlets/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewGeometryOutlets
+//
+//  Created by EnzoF on 26.08.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
